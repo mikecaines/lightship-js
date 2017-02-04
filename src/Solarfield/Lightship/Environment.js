02b2e2c1fd67e0a8d85ac3e04b32aadf2530ec44
mike <mikecaines@gmail.com>
2017-02-04 13:52:41 -0400

    remove forwardedChainLinks feature
    Not possible to selectively forward individual links and maintain the
     same order. Set the usual defaults at the js level. Fix some
     potential late static binding issues.

--- src/Solarfield/Lightship/Environment.js
+++ src/Solarfield/Lightship/Environment.js
@@ -2,9 +2,10 @@ define(
 	'solarfield/lightship-js/src/Solarfield/Lightship/Environment',
 	[
 		'solarfield/ok-kit-js/src/Solarfield/Ok/ObjectUtils',
+		'solarfield/ok-kit-js/src/Solarfield/Ok/StructUtils',
 		'solarfield/batten-js/src/Solarfield/Batten/Environment'
 	],
-	function (ObjectUtils, BattenEnvironment) {
+	function (ObjectUtils, StructUtils, BattenEnvironment) {
 		"use strict";
 
 		/**
@@ -15,6 +16,19 @@ define(
 		 */
 		const Environment = ObjectUtils.extend(BattenEnvironment);
 
+		Environment.getBaseChain = function () {
+			if (!Environment._sle_baseChain) {
+				Environment._sle_baseChain = BattenEnvironment.getBaseChain(this);
+				
+				Environment._sle_baseChain.splice(StructUtils.search(Environment._sle_baseChain, 'id', 'app'), 0, {
+					id: 'solarfield/lightship-js',
+					namespace: 'Solarfield.Lightship'
+				});
+			}
+			
+			return Environment._sle_baseChain.slice();
+		};
+		
 		ObjectUtils.defineNamespace('Solarfield.Lightship');
 		return Environment;
 	}
