96754ef267861261de26cddf32e0c5803783164e
mikecaines <mikecaines@gmail.com>
2018-03-17 14:45:05 -0300

    update Logger to support all RFC 5424 levels
    Calls to log with an unknown level, will fallback to the behaviour of
    the window.console.log method.

--- src/Solarfield/Lightship/Logger.js
+++ src/Solarfield/Lightship/Logger.js
@@ -6,19 +6,80 @@ define(
 		"use strict";
 		
 		/**
+		 * A logger that provides support for all RFC 5424 levels, and forwards to the global/browser console.
+		 * Calls to log with an unknown level, will fallback to the behaviour of the window.console.log method.
 		 * @class Solarfield.Lightship.Logger
+		 * @see https://tools.ietf.org/html/rfc5424#page-11
 		 */
-		var Logger = function () {};
-		
-		if (self.console && console.log) {
-			Logger.prototype.info = console.info ? function(m,c){if (arguments.length>1) console.info(m,c); else console.info(m)} : function(m,c){console.log(m,c)};
-			Logger.prototype.warn = console.warn ? function(m,c){if (arguments.length>1) console.warn(m,c); else console.warn(m)} : Logger.prototype.info;
-			Logger.prototype.error = console.error ? function(m,c){if (arguments.length>1) console.error(m,c); else console.error(m)} : Logger.prototype.info;
-			Logger.prototype.debug = console.debug ? function(m,c){if (arguments.length>1) console.debug(m,c); else console.debug(m)} : Logger.prototype.info;
-		}
-		else {
-			Logger.prototype.info = Logger.prototype.warn = Logger.prototype.error = Logger.prototype.debug = function(){};
-		}
+		var Logger = ObjectUtils.extend(null, {
+			log: function (aLevel, aMessage, aContext) {
+				var level = (''+aLevel).toUpperCase();
+				var funcName, funcArgs;
+				
+				if (level in this._sol_levels) {
+					funcName = this._sol_levels[level];
+					
+					funcArgs = [aMessage];
+					if (aContext !== undefined) funcArgs.push(aContext);
+				}
+				
+				else {
+					funcName = 'log';
+					funcArgs = arguments;
+				}
+				
+				console[funcName].apply(console, funcArgs);
+			},
+			
+			emergency: function (aMessage, aContext) {
+				this.log('EMERGENCY', aMessage, aContext);
+			},
+			
+			alert: function (aMessage, aContext) {
+				this.log('ALERT', aMessage, aContext);
+			},
+			
+			critical: function (aMessage, aContext) {
+				this.log('EMERGENCY', aMessage, aContext);
+			},
+			
+			error: function (aMessage, aContext) {
+				this.log('EMERGENCY', aMessage, aContext);
+			},
+			
+			warn: function (aMessage, aContext) {
+				this.log('WARNING', aMessage, aContext);
+			},
+			
+			warning: function (aMessage, aContext) {
+				this.warn(aMessage, aContext);
+			},
+			
+			notice: function (aMessage, aContext) {
+				this.log('NOTICE', aMessage, aContext);
+			},
+			
+			info: function (aMessage, aContext) {
+				this.log('INFO', aMessage, aContext);
+			},
+			
+			debug: function (aMessage, aContext) {
+				this.log('DEBUG', aMessage, aContext);
+			},
+			
+			constructor: function () {
+				this._sol_levels = {
+					EMERGENCY: 'error',
+					ALERT: 'error',
+					CRITICAL: 'error',
+					ERROR: 'error',
+					WARNING: 'warn',
+					NOTICE: 'info',
+					INFO: 'info',
+					DEBUG: 'debug',
+				};
+			},
+		});
 		
 		return Logger;
 	}
